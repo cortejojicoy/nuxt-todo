aaba46a00be3752c922700533bc38dc2a34fc608
cortejojicoy <jicoy.cortejo@gmail.com>
2024-05-21 12:06:22 +0800

    feat: update socialite controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -11,34 +11,38 @@ class AuthController extends Controller
 {
     public function redirectToProvider(Request $request)
     {
-        // return Socialite::driver($request->provider)->stateless()->redirect();
-        return Socialite::driver($request->provider)->scopes(['read:user', 'public_repo'])->stateless()->redirect();
+        return Socialite::driver('github')->stateless()->redirect();
     }
 
     public function handleProviderCallback(Request $request)
     {
-        $socialAccount = Socialite::driver($request->provider)->userFromToken($request->access_provider_token);
-        $user = User::firstOrCreate(
-            [
-                'social_id' => $socialAccount->id
-            ], [
-                'name' => $socialAccount->name ?? '',
-                'email' => $socialAccount->email,
-                'password' => $socialAccount->password ?? '',
-                'social_token' => $socialAccount->token,
-                'social_refresh_token' => $socialAccount->refreshToken ?? '',
-                'social_avatar' => $socialAccount->avatar,
-            ]
-        );
+        try {
+            $socialAccount = Socialite::driver('github')->stateless()->user();
 
-        $token = [
-            'token' => $user->createToken('sanctum+socialite')->plainTextToken,
-            'user' => $user
-        ];
+            // Check if the user already exists in your database
+            $user = User::where('email', $socialAccount->getEmail())->first();
+            if($user) {
+                Auth::login($user, true);
+            } else {
+                $user = User::create([
+                    'social_id' => $socialAccount->id,
+                    'name' => $socialAccount->name ?? '',
+                    'email' => $socialAccount->email,
+                    'password' => $socialAccount->password ?? '',
+                    'social_token' => $socialAccount->token,
+                    'social_refresh_token' => $socialAccount->refreshToken ?? '',
+                    'social_avatar' => $socialAccount->avatar,
+                ]);
+            }
+            $token = $user->createToken('sanctum+socialite')->plainTextToken;
+            return redirect( config('app.sanctum_stateful_domains').'/auth/callback?token='.$token);
+        }
+    }
 
-        return response()->json([
-            $token
-        ], 200);
+    public function user()
+    {
+        $user = User::where('email',Auth::user()->email)->first();
+        return response()->json([$user], 200);
     }
 
     public function logout(Request $request)
