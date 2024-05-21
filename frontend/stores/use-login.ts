1aef4265cd25d7d460550154da289ed414da7538
cortejojicoy <jicoy.cortejo@gmail.com>
2024-05-21 20:19:34 +0800

    feat: update store

--- frontend/stores/use-login.ts
+++ frontend/stores/use-login.ts
@@ -1,26 +1,38 @@
-import axios from 'axios';
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { APIAxios } from "~/shared/api";
 
 export const useLogin = defineStore("login", () => {
-    // state
-    const token = ref<string | null>(null)
-    const user = ref<object | null>(null)
+  // state
+  const route = useRoute();
+  const token = ref<string | null>(null);
+  const user = ref<object | null>(null);
+  const tempToken = route.query.tempToken as string;
 
-    // actions
-    const loginWithGithub = async () => {
-        try {
-            const { data } = await axios.get("http://localhost:8000/api/auth")
-            console.log(data)
-        } catch(e: unknown) {
-            console.log(e)
-        }
+  // actions
+
+  const loginWithGithub = () => {
+    window.location.href = "http://localhost:8000/api/auth";
+  };
+
+  const logout = () => {
+    
+  };
+
+  const getToken = async () => {
+    try {
+      const { data } = await APIAxios.get(`/get-token?tempToken=${tempToken}`);
+      token.value = data;
+    } catch (e: unknown) {
+      console.log(e);
     }
+  };
 
-    const login = () => {
-      window.location.href = "http://localhost:8000/api/auth";
-    }
+  const isAuthenticated = () => {
+    return !!token.value;
+  };
 
-    // return
-    return { loginWithGithub, login };
+  // return
+  return { loginWithGithub, getToken, token, isAuthenticated };
 });
