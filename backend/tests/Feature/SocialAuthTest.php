5e15cc6c8d13fda5dd43f6bb81d11387c15d7b11
cortejojicoy <jicoy.cortejo@gmail.com>
2024-05-22 11:02:35 +0800

    test: update social auth

--- backend/tests/Feature/SocialAuthTest.php
+++ backend/tests/Feature/SocialAuthTest.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
+use Mockery as m;
 
 class SocialAuthTest extends TestCase
 {
@@ -18,5 +21,23 @@ class SocialAuthTest extends TestCase
         $response = $this->get('/api/auth');
 
         $response->assertRedirect();
+    }
+
+    public function test_handle_provider_callback(): void
+    {
+        $socialiteUser = m::mock(SocialiteUser::class);
+        $socialiteUser->shouldReceive('getId')->andReturn(123456);
+        $socialiteUser->shouldReceive('getName')->andReturn('John Doe');
+        $socialiteUser->shouldReceive('getEmail')->andReturn('johndoe@example.com');
+
+        $socialite = m::mock('Laravel\Socialite\Contracts\Factory');
+        $socialite->shouldReceive('driver->user')->andReturn($socialiteUser);
+        $this->app->instance('Laravel\Socialite\Contracts\Factory', $socialite);
+
+        // Perform the GET request
+        $response = $this->get('/api/auth/callback');
+
+        $response->assertStatus(500);
+
     }
 }
